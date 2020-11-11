f8f5a2676b0d8f6b6a53b19c04adc11c1a94c7b5
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-11-11 10:45:53 +0000

    Pass the setInputText to the Form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
+import TodoList from './components/TodoList';
+import { useState } from 'react';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+
   return (
     <div className="App">
       <h1>Hello React</h1>
       <header>Mouha's Todo List</header>
-      <Form />
+      <Form setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
